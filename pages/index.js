c46bac9f8096d252b8dd4ad34b20b2f1a2153c1f
Karthik P V <karthik.padav@tecnotree.com>
2020-10-14 21:23:29 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import React from "react";
+import Home from "./Home/Home";
+import Header from "./NavBar/Header";
+
+class App extends React.Component {
+  static async getInitialProps(ctx) {
+    const res = await fetch(
+      "http://localhost:8080/qa_react_wordpress/wp-json/api/v1/questions"
+    );
+    const json = await res.json();
+    const isServer = typeof window === "undefined";
+    return { json, isServer };
+  }
+
+  async componentDidMount() {
+    const res = await fetch("https://api.github.com/repos/vercel/next.js");
+    const json = await res.json();
+    console.log(json, "we are in did mount");
+  }
+
+  render() {
+    console.log(this.props, "this.props123");
+    return (
+      <>
+        <Header />
+        <Home />
+      </>
+    );
+  }
+}
+
+export default App;
