cca15fee2ddd6fcda5ee93d1c04f926dfc7f9068
Karthik P V <karthik.padav@tecnotree.com>
2020-10-15 20:49:09 +0530

    .

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import TabsComponent from "./TabsComponent";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,9 +25,7 @@ export default function Home() {
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
         <Grid item xs={6} className={classes.p1}>
-          <Paper className={classes.paper}>
-            <TabsComponent />
-          </Paper>
+          <Paper className={classes.paper}>asdasd></Paper>
         </Grid>
         <Grid item xs={3} className={classes.p1}>
           <Paper className={classes.paper}>xs=6</Paper>
