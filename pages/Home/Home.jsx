f2c6f14430eb77c89f3d320bafd6ac7883647d2d
Karthik P V <karthik.padav@tecnotree.com>
2020-10-15 20:24:11 +0530

    material ui

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,6 +1,39 @@
-import Head from "next/head";
-import styles from "../../styles/Home.module.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TabsComponent from "./TabsComponent";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  p1: {
+    padding: theme.spacing(1),
+  },
+}));
 
 export default function Home() {
-  return <p>Home</p>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={3} className={classes.p1}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6} className={classes.p1}>
+          <Paper className={classes.paper}>
+            <TabsComponent />
+          </Paper>
+        </Grid>
+        <Grid item xs={3} className={classes.p1}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
--- pages/NavBar/Header.jsx
+++ pages/NavBar/Header.jsx
@@ -1,33 +1,43 @@
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Header() {
+  const classes = useStyles();
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          {/* <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form> */}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
