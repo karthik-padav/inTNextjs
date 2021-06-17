e2c8221b9e02697bf718c8f2a3572f22e11ab4b4
Karthik Padav <karthik.padav@tecnotree.com>
2021-06-17 16:02:50 +0530

    env variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    S3_BASE_URL: "https://in-tulunadu-files.s3.ap-south-1.amazonaws.com/",
-    SERVER_1_BASEURL: "https://in-tulunadu.herokuapp.com",
+    S3_BASE_URL: process.env.S3_BASE_URL,
+    SERVER_1_BASEURL: process.env.SERVER_1_BASEURL,
     GOOGLE_AUTH_CLIENTID: process.env.GOOGLE_AUTH_CLIENTID,
   },
 };
